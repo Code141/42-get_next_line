2f460fb80a36a1f3dec0bd5cbfb4da00c4725ed0
Code141 <code141@iMac-de-Code141.local>
2017-11-24 08:59:17 +0100

    c141 modify

--- c141/c141_hr.c
+++ c141/c141_hr.c
@@ -1,24 +1,24 @@
 #include "libft.h"
 
-void	c141_hr(char c, int len)
+static void	c141_hr_padded_str(char *str, char c, int len)
+{
+	ft_putchar(c);
+	ft_putchar(' ');
+	ft_putstr(str);
+	while ((len--) - (ft_strlen(str) + 3))	
+			ft_putchar(' ');
+	ft_putchar(c);
+	ft_putchar('\n');
+}
+
+void		c141_hr(char c, int len)
 {
 	while (len--)
 		ft_putchar(c);
 	ft_putchar('\n');
 }
 
-void	c141_hr_padded_str(char *str, char c, int len)
-{
-	ft_putchar(c);
-	ft_putchar(' ');
-	ft_putstr(str);
-	while ((len--) - (ft_strlen(str) + 3))	
-		ft_putchar(' ');
-	ft_putchar(c);
-	ft_putchar('\n');
-}
-
-void	c141_hr_banner(char *name, char motif, int len)
+void		c141_hr_banner(char *name, char motif, int len)
 {
 	ft_putchar('\n');
 	c141_hr(motif, len);
@@ -26,7 +26,7 @@ void	c141_hr_banner(char *name, char motif, int len)
 	c141_hr(motif, len);
 }
 
-void	c141_hr_banner_project(char *name, char motif, int len)
+void		c141_hr_banner_project(char *name, char motif, int len)
 {
 	c141_hr(motif, len);
 	c141_hr_padded_str(name, motif, len);
