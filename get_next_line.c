751f6246d4725ad697dff50b195a187e2911f49e
Geoffrey LAMBINET <gelambin@student.42.fr>
2017-12-05 14:55:17 +0100

    repush clean

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gelambin <gelambin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 13:24:01 by gelambin          #+#    #+#             */
-/*   Updated: 2017/12/05 12:49:59 by gelambin         ###   ########.fr       */
+/*   Updated: 2017/12/05 14:52:53 by gelambin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,6 @@
 void    ft_list_print(t_list *list);
 t_list	*ft_lst_join(t_list *alst, t_list *blist);
 t_list	*ft_lst_compact(t_list *alst);
-
-
-
-
-
-
-
 
 t_file	*get_dial(int fd, t_list **dial)
 {
@@ -73,7 +66,6 @@ int		read_more(t_file *file)
 				// FREE FILE SAVE
 				file->save = new;
 			}
-			file->line = (char*)(file->save->content);
 			return (1);
 		}
 	}
@@ -82,19 +74,31 @@ int		read_more(t_file *file)
 	return (-1);
 }
 
-int		fill(t_file *file, char **line)
+int		send(t_file *file, char **line)
 {
-	return (0);
+	int		i;
+	char	*str;
+	t_list	*new;
+
+	str = ((char*)(file->save->content));
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	*line = (char*)malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (-1);
+	i = 0;
+	while (str[i] != '\n')
+	{
+		(*line)[i] = str[i];
+		i++;
+	}
+	(*line)[i] = '\0';
+	new = ft_lstnew(((char*)(file->save->content)) + i + 1, file->save->content_size - i - 1);
+	//FREE FILE->SAVE
+	file->save = new;
+	return (1);
 }
-
-
-
-
-
-
-
-
-
 
 int		get_next_line(const int fd, char **line)
 {
@@ -104,43 +108,19 @@ int		get_next_line(const int fd, char **line)
 	file = get_dial(fd, &dial);
 	if (!file)
 		return (-1); // allocation probleme dans dial
-	printf("-| LOADED FD = %d |----------------\n", file->fd);
+
 	if (!file->save || !ft_memchr(file->save->content, '\n', file->save->content_size))
 		read_more(file);
 
-	int				i;
-	char			*str;
-
-	if (file->line)
-		str = file->line;
-	else
-		str = (char*)(file->save->content);
-	i = 0;
-	while (str[i] != '\n')
-		i++;
-	line = (char**)malloc(sizeof(*line) * (i + 1));
-	if (!line)
-		return (-1);
-	i = 0;
-	while (str[i] != '\n')
-	{
-		(*line)[i] = str[i];
-		i++;
-	}	
-		
-		
-		
-		
-	//	fill(file, line);
+	send(file, line);
 
 /*-------------------------------------------------------------------------*/	
 
 //	ft_list_print(file->save);
-/*
+
 	t_list	*file_l;
 	t_list	*save;
-	int		i;
-	
+/*	
 	file_l = dial;
 	while (file_l)
 	{
@@ -150,6 +130,7 @@ int		get_next_line(const int fd, char **line)
 		while (save)
 		{
 			i = 0;
+			
 			while (i < save->content_size)
 					ft_putchar(((char*)save->content)[i++]);
 			save = save->next;
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,19 @@
 /*   By: gelambin <gelambin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 13:23:38 by gelambin          #+#    #+#             */
-/*   Updated: 2017/12/05 12:41:12 by gelambin         ###   ########.fr       */
+/*   Updated: 2017/12/05 14:51:38 by gelambin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "libft/libft.h"
-# define BUFF_SIZE 20000
+# define BUFF_SIZE 20
 
 typedef struct	s_file
 {
-	int		fd;
-	t_list	*save;
-	char	*line;
+	int				fd;
+	t_list			*save;
 }				t_file;
 
 int				get_next_line(const int fd, char **line);
